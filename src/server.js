471020fa316650ab76c01d99aa60bbe93da14a25
Roman lesyo <ladiginscormag@gmail.com>
2023-03-17 19:09:30 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,18 +5,20 @@ const app = require("./app");
 
 mongoose.set("strictQuery", false);
 
-const { MONGODB_HOST_URI } = process.env;
-const { PORT } = process.env;
+// const { MONGODB_HOST_URI } = process.env;
+// const { PORT } = process.env;
 
 async function main() {
   try {
-    await mongoose.connect(MONGODB_HOST_URI);
+    await mongoose.connect(
+      "mongodb+srv://admin:vTAoAaxp0Obq9P8E@petly.hofegc9.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("Database connection successful");
-    app.listen(PORT, (err) => {
+    app.listen(8080, (err) => {
       if (err) {
         console.log("Error ", err);
       }
-      console.log(`Server is running. Use our API on port: ${PORT}`);
+      console.log(`Server is running. Use our API on port: 8080`);
     });
   } catch (error) {
     console.error("Error while connecting to mongodb", error.message);
