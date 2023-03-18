7736df669465ed3e9d28874cc48d816f7d746d98
Roman lesyo <ladiginscormag@gmail.com>
2023-03-18 09:46:01 +0200

    added auth controlers

---
+++ src/controllers/users/usersControlers.js
@@ -0,0 +1,204 @@
+const { User } = require("../../schemas/user");
+const { authSchema } = require("../../schemas/joi");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const { refreshSchema } = require("../../schemas/joi");
+const sendEmail = require("../../services/sendEmail");
+
+const { ACCESS_SECRET_KEY, REFRESH_SECRET_KEY } = process.env;
+
+async function signup(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const { error } = authSchema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: "Invalid value of email or password" });
+      return;
+    }
+    const userCheck = await User.findOne({ email });
+    if (userCheck) {
+      res.status(409).json({ message: "Email in use" });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const verificationToken = uuidv4();
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+      verificationToken,
+      verify: true,
+    });
+    // await sendEmail(email, verificationToken);
+    return res.status(201).json({
+      status: "success",
+      code: 201,
+      user: {
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const { error } = authSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: "Wrong email or password" });
+    }
+
+    const user = await User.findOne({ email });
+    const userPassword = await bcrypt.compare(password, user.password);
+    if (!user || !userPassword) {
+      return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    if (!user.verify) {
+      return res.status(401).json({ message: "Your Email is not verifyied!" });
+    }
+
+    const payload = {
+      id: user._id,
+    };
+    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    await User.findByIdAndUpdate(user._id, { accessToken, refreshToken });
+
+    return res.status(200).json({
+      status: "success",
+      code: 200,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      user: {
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+const refresh = async (req, res, next) => {
+  try {
+    const { refreshToken: token } = req.body;
+    const { REFRESH_SECRET_KEY, ACCESS_SECRET_KEY } = process.env;
+    const { error } = refreshSchema.validate(req.body);
+
+    if (error) {
+      return res.status(403).json({ message: "Wrong token" });
+    }
+    const { id } = jwt.verify(token, REFRESH_SECRET_KEY);
+    const isExist = await User.findOne({ refreshToken: token });
+    if (!isExist) {
+      return res.status(403).json({ message: "Token is not valid" });
+    }
+
+    const payload = {
+      id,
+    };
+
+    const accessToken = jwt.sign(payload, ACCESS_SECRET_KEY, {
+      expiresIn: "2m",
+    });
+    const refreshToken = jwt.sign(payload, REFRESH_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    return res.status(200).json({
+      accessToken,
+      refreshToken,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+async function logout(req, res, next) {
+  try {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, {
+      accessToken: null,
+      refreshToken: null,
+    });
+    return res.status(204).json(); // "Logout was successfull"
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function current(req, res, next) {
+  try {
+    const { email } = req.user;
+    return res.status(200).json({
+      user: {
+        email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function resendEmail(req, res, next) {
+  try {
+    const { email } = req.body;
+    const verificationToken = uuidv4();
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "missing required field email" });
+    }
+
+    if (user.verify) {
+      return res
+        .status(400)
+        .json({ message: "Verification has been already passed" });
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verificationToken,
+    });
+
+    await sendEmail(email, verificationToken);
+    return res.status(200).json({ message: "Verification email sended" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+async function verifyEmail(req, res) {
+  const { verificationToken } = req.params;
+
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: null,
+    verify: true,
+  });
+  return res.status(200).json({ message: "Verification successful" });
+}
+
+module.exports = {
+  current,
+  signup,
+  verifyEmail,
+  resendEmail,
+  refresh,
+  login,
+  logout,
+};
