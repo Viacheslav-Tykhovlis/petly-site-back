eaa2ab16fa1c5b76aad143685e1432d3cbb62c4b
Roman lesyo <ladiginscormag@gmail.com>
2023-03-18 11:45:52 +0200

    Update servicesRouter.js

--- src/routes/api/servicesRouter.js
+++ src/routes/api/servicesRouter.js
@@ -11,21 +11,21 @@ const {
   addNoticesSelected,
   addNew,
   sponsors,
-  news
+  news,
   // noticesSelected,
 } = require("../../controllers/notices/noticesControlers");
-// const { authMiddleware } = require("../../middlewares/authMiddleware");
+const { authMiddleware } = require("../../middlewares/authMiddleware");
 
 const servicesRouter = express.Router();
 // servicesRouter.use(authMiddleware);
 
 // 1. створити ендпоінт для отримання контактної інформації про сервіси які надають послуги(захаркодить перелік новин в базу данних)
 // 2. створити ендпоінт для отримання новин(захаркодить перелік новин в базу данних)
-servicesRouter.get("/servicessidebar", servicesSidebar);
-servicesRouter.get("/news", news);
-servicesRouter.get("/sponsors", sponsors);
-servicesRouter.post("/new", addNew);
-servicesRouter.post("/sponsor", addSponsor);
+servicesRouter.get("/servicessidebar", authMiddleware, servicesSidebar);
+servicesRouter.get("/news", authMiddleware, news);
+servicesRouter.get("/sponsors", authMiddleware, sponsors);
+servicesRouter.post("/new", authMiddleware, addNew);
+servicesRouter.post("/sponsor", authMiddleware, addSponsor);
 
 // 3. створити ендпоінт для пошуку оголошеннь по заголовку
 // 5. створити ендпоінт для отримання одного оголошення
@@ -37,15 +37,15 @@ servicesRouter.post("/sponsor", addSponsor);
 // 6. створити ендпоінт для додавання оголошення до обраних
 // 11. створити ендпоінт для видалення оголошення авторизованого користувача створеного цим же користувачем
 
-servicesRouter.get("/noticesByTitle", noticesByTitle);
-servicesRouter.get("/notices", noticesByCategory);
-servicesRouter.post("/notices", addNotices);
-servicesRouter.delete("/:noticesId", deleteNoticesByID);
-servicesRouter.post("/notices_selected", addNoticesSelected);
+servicesRouter.get("/noticesByTitle", authMiddleware, noticesByTitle);
+servicesRouter.get("/notices", authMiddleware, noticesByCategory);
+servicesRouter.post("/notices", authMiddleware, addNotices);
+servicesRouter.delete("/:noticesId", authMiddleware, deleteNoticesByID);
+servicesRouter.post("/notices_selected", authMiddleware, addNoticesSelected);
 
 // 12. створити ендпоінт для отримання  особистої інформації про користувача, з коллекції users
 // 13. інформації про тварин корисувача, з коллекції pets.
-servicesRouter.get("/user/about", aboutUser);
-servicesRouter.get("/user/aboutuserPets", aboutUserPets);
+servicesRouter.get("/user/about", authMiddleware, aboutUser);
+servicesRouter.get("/user/aboutuserPets", authMiddleware, aboutUserPets);
 
 module.exports = servicesRouter;
