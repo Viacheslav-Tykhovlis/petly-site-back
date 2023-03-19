0485344b458d2714410a926d05cb661546d14376
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2023-03-19 12:44:08 +0100

    Update server.js
    1. Обновлена настройка порта подключения

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = require("./app");
 mongoose.set("strictQuery", false);
 
 // const { MONGODB_HOST_URI } = process.env;
-// const { PORT } = process.env;
+const PORT = process.env.PORT || 8080;
 
 async function main() {
   try {
@@ -14,11 +14,11 @@ async function main() {
       "mongodb+srv://admin:vTAoAaxp0Obq9P8E@petly.hofegc9.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("Database connection successful");
-    app.listen(8080, (err) => {
+    app.listen(PORT, (err) => {
       if (err) {
         console.log("Error ", err);
       }
-      console.log(`Server is running. Use our API on port: 8080`);
+      console.log(`Server is running. Use our API on port: ${PORT}`);
     });
   } catch (error) {
     console.error("Error while connecting to mongodb", error.message);
