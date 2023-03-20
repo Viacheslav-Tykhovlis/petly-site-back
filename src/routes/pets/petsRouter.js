acc9c7cbe3322d5861872d3a02cd0c179313a0e9
Oksana Konieva <anaska0489@gmail.com>
2023-03-20 19:42:31 +0200

    fix

---
+++ src/routes/pets/petsRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const {
+  getAll,
+  addPet,
+  removeById,
+} = require("../../controllers/pets/petsControlers");
+
+const { ctrlWrapper } = require("../../middlewares/ctrlWrapper");
+
+const petsRouter = express.Router();
+
+const { joiSchema } = require("../../schemas/pet");
+
+petsRouter.get("/", ctrlWrapper(getAll));
+
+petsRouter.post("/pet", ctrlWrapper(addPet));
+
+petsRouter.delete("/:petId", ctrlWrapper(removeById));
+
+module.exports = petsRouter;
