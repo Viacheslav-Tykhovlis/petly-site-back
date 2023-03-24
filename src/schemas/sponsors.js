1a9c1dfbb5fffc4352268107b3593557d5b400e1
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2023-03-24 16:09:49 +0100

    Fix schema sponsors

--- src/schemas/sponsors.js
+++ src/schemas/sponsors.js
@@ -1,45 +1,45 @@
 const { Schema, model, ObjectId } = require("mongoose");
 
-const schemaSponsors = new Schema({
-  _id: {
-    type: ObjectId,
-  },
-  title: {
-    type: String,
-  },
-  url: {
-    type: String,
-  },
-  imageUrl: {
-    type: String,
-  },
-  addressUrl: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  workDays: [
-    {
-      _id: {
-        type: ObjectId,
-      },
-      from: {
-        type: String,
-      },
-      to: {
-        type: String,
-      },
-      isOpen: { type: Boolean },
+const schemaSponsors = new Schema(
+  {
+    _id: {
+      type: ObjectId,
     },
-  ],
-});
+    title: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+    },
+    addressUrl: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    workDays: [
+      {
+        from: {
+          type: String,
+        },
+        to: {
+          type: String,
+        },
+        isOpen: { type: Boolean },
+      },
+    ],
+  },
+  { versionKey: false }
+);
 
 const Sponsors = model("sponsors", schemaSponsors);
 
