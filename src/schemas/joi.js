469e13ce0f467a637ea81b5a0b52b1ffeccb22a5
Roman lesyo <ladiginscormag@gmail.com>
2023-03-24 12:12:54 +0200

    Update joi.js

--- src/schemas/joi.js
+++ src/schemas/joi.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const authSchema = Joi.object({
+  name: Joi.string(),
   password: Joi.string().trim().min(7).max(32).required(),
   email: Joi.string().trim().email().required(),
 });
