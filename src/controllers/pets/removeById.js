b6c56c0dc7d830a176dc59d6868e99c39adbafbc
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2023-03-30 10:05:45 +0200

    Update removeById.js

--- src/controllers/pets/removeById.js
+++ src/controllers/pets/removeById.js
@@ -3,19 +3,18 @@ const { NotFound } = require("http-errors");
 
 const removeById = async (req, res) => {
   const { petId } = req.params;
+  const owner = req.user.id;
 
   const result = await Pet.findByIdAndRemove(petId, req.body);
 
   if (!result) {
     throw new NotFound(`Pet with id = ${petId} not found`);
   }
-  res.json({
-    status: "success",
-    code: 200,
-    data: {
-      result,
-    },
-  });
+  const allUserPets = await Pet.find(
+    { owner: owner },
+    { name: 1, birthday: 1, breed: 1, photo: 1, comments: 1 }
+  );
+  res.json({ success: true, allUserPets });
 };
 
 module.exports = removeById;
