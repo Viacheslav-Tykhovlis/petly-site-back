a42bb512cb23a86ead3bdc039e434702edf08a5f
Roman lesyo <ladiginscormag@gmail.com>
2023-03-31 13:12:03 +0300

    Update signup.js

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,30 +1,24 @@
 const bcrypt = require("bcrypt");
-const { v4: uuidv4 } = require("uuid");
 const { User } = require("../../schemas/user");
-const { authSchema } = require("../../schemas/joi");
-// const sendEmail = require("../../services/sendEmail");
-
 
 async function signup(req, res) {
-  const { email, password, phone, birthday, name, avatarUrl, city } = req.body;
+  const { email, password } = req.body;
   const emailToLoWerCase = email.toLowerCase();
+  try {
+    const userCheck = await User.findOne({ email: emailToLoWerCase });
+    if (userCheck) {
+      res.status(409).json({ message: "Email in use" });
+      return;
+    }
 
-  const userCheck = await User.findOne({ email: emailToLoWerCase });
-  if (userCheck) {
-    res.status(409).json({ message: "Email in use" });
-    return;
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = await User.create({
       email,
       password: hashedPassword,
-      verificationToken,
-      verify: true,
     });
-    // await sendEmail(email, verificationToken);
+
     return res.status(201).json({
       status: "success",
       code: 201,
@@ -38,4 +32,3 @@ async function signup(req, res) {
 }
 
 module.exports = signup;
-
